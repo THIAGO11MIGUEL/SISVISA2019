149453b55974b8c08a9d62d8cd14b659ed5f1a96
THIAGO11MIGUEL <50973867+THIAGO11MIGUEL@users.noreply.github.com>
2019-05-23 22:33:01 -0400

    FONTES DO PROJETO SISVISA MIGRADOS NO DIA 23/05/019

---
+++ SISVISA.dpr
@@ -0,0 +1,25 @@
+program SISVISA;
+
+uses
+  FMX.Forms,
+  U_SISVISA in 'units\U_SISVISA.pas' {frmSISVISA},
+  U_CadastroDenuncias in 'units\U_CadastroDenuncias.pas',
+  U_CADASTROPADRAO in 'units\U_CADASTROPADRAO.pas' {frmCadastroPadrao},
+  U_CadastroReceitas in 'units\U_CadastroReceitas.pas' {frmCadastroReceitas},
+  U_dmSISVISA in 'units\U_dmSISVISA.pas' {dmSISVISA: TDataModule},
+  U_CadastroProcADM in 'units\U_CadastroProcADM.pas' {frmCadastroProcADM},
+  U_MensagemPadrao in 'units\U_MensagemPadrao.pas' {frmMensagemPadrao},
+  Classes.Utils.View in 'classes\Classes.Utils.View.pas',
+  U_CadastroArtigos in 'units\U_CadastroArtigos.pas' {frmCadastroArtigos},
+  U_Configuracoes in 'units\U_Configuracoes.pas' {frmConfiguracoesGerais},
+  U_CadastroBD in 'units\U_CadastroBD.pas' {frmCadastroBD},
+  SISVISA.Model.CaminhoBD in 'classes\SISVISA.Model.CaminhoBD.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmSISVISA, frmSISVISA);
+  Application.CreateForm(TdmSISVISA, dmSISVISA);
+  Application.Run;
+end.
